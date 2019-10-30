b3d3c395e53123979fed8aa57c45e020e0e0190e
NicklasPekkarinen <56152106+nicklaspekkarinen@users.noreply.github.com>
2019-10-30 13:17:12 +0100

    Stjärt

--- app/src/main/java/com/autorave/chatapp/MainActivity.java
+++ app/src/main/java/com/autorave/chatapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hej Chatappen1337!");
+        System.out.println("Stjärt");
     }
 }
