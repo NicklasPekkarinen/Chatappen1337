5cf45bc2df8c9c5c054679a8f7c12c1a6e4980a5
MarcusNilsson93 <marcus@marcuss-macbook-air.local>
2019-10-30 14:49:52 +0100

    Hej
    Signed-off-by: MarcusNilsson93 <marcus@Marcuss-MacBook-Air.local>

--- app/src/main/java/com/autorave/chatapp/MainActivity.java
+++ app/src/main/java/com/autorave/chatapp/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hej Chatappen13377!");
+        System.out.println("LOL");
     }
 }
