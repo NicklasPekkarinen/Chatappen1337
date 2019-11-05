2755e09642a822554343dd5de78653a2efb45100
NicklasPekkarinen <56152106+nicklaspekkarinen@users.noreply.github.com>
2019-11-05 11:21:07 +0100

    Main page updated

--- app/src/main/java/com/autorave/chatapp/MainActivity.java
+++ app/src/main/java/com/autorave/chatapp/MainActivity.java
@@ -1,16 +1,73 @@
 package com.autorave.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements ChatsFragment.OnFragmentInteractionListener {
+
+    private FrameLayout frameLayout;
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hej Chatappen13377!");
-        System.out.println("LOL");
+
+        bottomNav = findViewById(R.id.bottom_nav);
+        frameLayout = findViewById(R.id.frame_layout);
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new ChatsFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()) {
+                case R.id.nav_chats:
+                    selectedFragment = new ChatsFragment();
+                    break;
+                case R.id.nav_groups:
+                    selectedFragment = new GroupChatsFragment();
+                    break;
+                case R.id.nav_contacts:
+                    selectedFragment = new ContactsFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, selectedFragment, null).commit();
+
+            return true;
+        }
+    };
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
