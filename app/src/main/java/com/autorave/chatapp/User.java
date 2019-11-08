f3871583c903fb73f05a99e181ecd4d9f69f921a
MarcusNilsson93 <marcus@marcuss-macbook-air.local>
2019-11-08 20:27:28 +0100

    created userclass that holds your username

---
+++ app/src/main/java/com/autorave/chatapp/User.java
@@ -0,0 +1,19 @@
+package com.autorave.chatapp;
+
+    public class User {
+
+        public String username;
+
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot
+        }
+
+        public User(String username) {
+            this.username = username;
+
+        }
+
+    }
+
+
