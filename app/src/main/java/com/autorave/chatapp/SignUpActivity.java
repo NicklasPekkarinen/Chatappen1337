53db8833f6841d5b375ea6d723e74f07bc15f481
MarcusNilsson93 <marcus@marcuss-macbook-air.local>
2019-11-08 20:25:08 +0100

    added Username to Realtime database

--- app/src/main/java/com/autorave/chatapp/SignUpActivity.java
+++ app/src/main/java/com/autorave/chatapp/SignUpActivity.java
@@ -5,33 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class SignUpActivity extends AppCompatActivity {
-    EditText emailID, passWord;
-    FirebaseAuth mFirebaseAuth;
+    private EditText emailID, passWord, userName;
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mRootReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mFirebaseAuth =FirebaseAuth.getInstance();
+        if(mFirebaseAuth.getCurrentUser() == null){
+            finish();
+            startActivity(new Intent(this,LoginActivity.class));
+        }
+        mRootReference = FirebaseDatabase.getInstance().getReference();
+
         emailID = findViewById(R.id.edit_email);
         passWord = findViewById(R.id.edit_password);
+        userName = findViewById(R.id.user_name);
+
+
 
     }
+
     public void signUp(View view) {
+        mFirebaseAuth = FirebaseAuth.getInstance();
         String email = emailID.getText().toString();
         String pwd = passWord.getText().toString();
-        mFirebaseAuth = FirebaseAuth.getInstance();
+
 
         if(email.isEmpty()){
             emailID.setError("Please enter your email");
@@ -41,16 +62,18 @@ public class SignUpActivity extends AppCompatActivity {
             passWord.setError("Please enter your password");
             passWord.requestFocus();
         }
+
         else if(!(email.isEmpty() && pwd.isEmpty())){
             mFirebaseAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(!task.isSuccessful()){
-                        Toast.makeText(SignUpActivity.this,"SignUp unsuccessful",Toast.LENGTH_SHORT).show();
+                    if(task.isSuccessful()){
+                        insertValuesToFirebase();
+                        FirebaseUser user = mFirebaseAuth.getCurrentUser();
 
                     }
                     else{
-                        startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+                        Toast.makeText(SignUpActivity.this,"SignUp failed",Toast.LENGTH_SHORT).show();
 
                     }
                 }
@@ -62,10 +85,24 @@ public class SignUpActivity extends AppCompatActivity {
         }
 
     }
+
+
+
+    private void insertValuesToFirebase(){
+        String uN = userName.getText().toString().trim();
+        User userName = new User(uN);
+        FirebaseUser user = mFirebaseAuth.getCurrentUser();
+        mRootReference.child(user.getUid()).setValue(userName);
+
+        Toast.makeText(SignUpActivity.this,"Account Created, Welcome :)",Toast.LENGTH_SHORT).show();
+
+    }
+
     public void backToLogin(View view){
         Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
         startActivity(intent);
 
     }
+
 
 }
