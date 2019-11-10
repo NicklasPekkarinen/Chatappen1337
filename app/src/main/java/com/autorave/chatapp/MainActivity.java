51dca2db6ab3ee8e158cae822d06abc3f185c232
NicklasPekkarinen <56152106+nicklaspekkarinen@users.noreply.github.com>
2019-11-10 19:57:48 +0100

    Added databasereference to set username to that of the current user

--- app/src/main/java/com/autorave/chatapp/MainActivity.java
+++ app/src/main/java/com/autorave/chatapp/MainActivity.java
@@ -10,14 +10,28 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements ChatsFragment.OnFragmentInteractionListener {
 
     private FrameLayout frameLayout;
     private BottomNavigationView bottomNav;
+    private CircleImageView profileImage;
+    private TextView userName;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,25 @@ public class MainActivity extends AppCompatActivity implements ChatsFragment.OnF
 
         bottomNav = findViewById(R.id.bottom_nav);
         frameLayout = findViewById(R.id.frame_layout);
+        profileImage = findViewById(R.id.profile_image);
+        userName = findViewById(R.id.username_toolbar);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child(firebaseUser.getUid());
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                userName.setText(user.getUsername());
+                profileImage.setImageResource(R.mipmap.ic_launcher);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
