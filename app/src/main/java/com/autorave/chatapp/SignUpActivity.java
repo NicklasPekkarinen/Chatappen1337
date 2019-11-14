8b98b9b6118eda56062bdf2a734801fc915065ed
MarcusNilsson93 <marcus@Marcuss-MacBook-Air.local>
2019-11-14 17:04:36 +0100

    cleaned up code, added comments and implemented Email verification

--- app/src/main/java/com/autorave/chatapp/SignUpActivity.java
+++ app/src/main/java/com/autorave/chatapp/SignUpActivity.java
@@ -24,12 +24,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
-
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText emailID, passWord, userName;
-    private CheckBox showPassword;
     private FirebaseAuth mFirebaseAuth;
     private DatabaseReference mRootReference;
 
@@ -39,13 +36,12 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         mFirebaseAuth =FirebaseAuth.getInstance();
-
         mRootReference = FirebaseDatabase.getInstance().getReference();
 
         emailID = findViewById(R.id.edit_email);
         passWord = findViewById(R.id.edit_password);
         userName = findViewById(R.id.user_name);
-        showPassword = findViewById(R.id.check_box);
+        CheckBox showPassword = findViewById(R.id.check_box);
 
         showPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -60,11 +56,13 @@ public class SignUpActivity extends AppCompatActivity {
         });
 
     }
+    //Sign up method
     public void signUp(View view) {
         mFirebaseAuth = FirebaseAuth.getInstance();
         String email = emailID.getText().toString();
         String pwd = passWord.getText().toString();
 
+        //checks if any of the fields are empty
         if(email.isEmpty()){
             emailID.setError("Please enter your email");
             emailID.requestFocus();
@@ -73,7 +71,7 @@ public class SignUpActivity extends AppCompatActivity {
             passWord.setError("Please enter your password");
             passWord.requestFocus();
         }
-        else if(!(email.isEmpty() && pwd.isEmpty())){
+            // If all field are field this method crates user
             mFirebaseAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
@@ -87,16 +85,11 @@ public class SignUpActivity extends AppCompatActivity {
                     }
                 }
             });
-        }
-        else {
-            Toast.makeText(SignUpActivity.this,"Error occurred",Toast.LENGTH_SHORT).show();
-        }
-
     }
-
+    // Method to insert all values in database, like Email and password and username
     private void insertValuesToFirebase(){
         String uN = userName.getText().toString().trim();
-        FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+        final FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
         String userID = firebaseUser.getUid();
         User user = new User(uN,userID);
 
@@ -104,17 +97,33 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if(task.isSuccessful()){
-                    Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
-                    startActivity(intent);
-                    finish();
+                    FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                    if (user != null) {
+
+                        //Email verification sent
+                        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(SignUpActivity.this,"Account Created, Please verify your Email ",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    Toast.makeText(SignUpActivity.this,"Sign up failed, Please try again ",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                        //back to Login screen after successful sign up
+                        Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
                 }
             }
         });
 
-
-        Toast.makeText(SignUpActivity.this,"Account Created, Welcome :)",Toast.LENGTH_SHORT).show();
     }
-
     public void backToLogin(View view){
         Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
         startActivity(intent);
