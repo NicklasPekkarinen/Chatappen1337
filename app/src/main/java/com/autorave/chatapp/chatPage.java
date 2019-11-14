36b36825ebb4f35531c0282d0ab1fb3ae1834195
Petteri Tuononen <monika.musak@gmail.com>
2019-11-14 22:42:53 +0100

    adding chat features

--- app/src/main/java/com/autorave/chatapp/chatPage.java
+++ app/src/main/java/com/autorave/chatapp/chatPage.java
@@ -1,14 +1,137 @@
 package com.autorave.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class chatPage extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ChatPage extends AppCompatActivity {
+
+    CircleImageView profile_image;
+    TextView username;
+
+    ImageButton btnSend;
+    EditText textSend;
+
+    ChatAdapter chatAdapter;
+    List<ChatInfo> mchat;
+
+    RecyclerView recyclerView;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_page);
+
+        recyclerView = findViewById(R.id.id_text);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        profile_image = findViewById(R.id.profile_image);
+        username = findViewById(R.id.chats_username);
+        btnSend = findViewById(R.id.send_button);
+        textSend = findViewById(R.id.message_send);
+
+        Intent intent = getIntent();
+        final String userId = intent.getStringExtra("userId");
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = textSend.getText().toString();
+                if(!msg.equals("")){
+                    sendMessage(firebaseUser.getUid(),userId,msg);
+                }else{
+                    Toast.makeText(ChatPage.this, "Type in a message", Toast.LENGTH_SHORT).show();
+                }
+                textSend.setText("");
+
+                }
+
+        });
+
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                username.setText(user.getUsername());
+                readMessage(firebaseUser.getUid(), userId);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void sendMessage(String sender, String receiver, String message){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
+
+    }
+
+    private void readMessage(final String myid, final String userid){
+        mchat = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mchat.clear();
+                for(DataSnapshot snapshot :dataSnapshot.getChildren()){
+                    ChatInfo chatInfo = snapshot.getValue(ChatInfo.class);
+                    if(chatInfo.getReceiver().equals(myid) && chatInfo.getSender().equals(userid) ||
+                            chatInfo.getReceiver().equals(userid) && chatInfo.getSender().equals(myid)){
+                        mchat.add(chatInfo);
+                    }
+
+                    chatAdapter = new ChatAdapter(ChatPage.this, mchat);
+                    recyclerView.setAdapter(chatAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
