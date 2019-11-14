95995f2f0bf0711711efc8d2f9e191a0c7e8a016
NicklasPekkarinen <56152106+nicklaspekkarinen@users.noreply.github.com>
2019-11-14 11:21:10 +0100

    Updated contacts fragment java

--- app/src/main/java/com/autorave/chatapp/ContactsFragment.java
+++ app/src/main/java/com/autorave/chatapp/ContactsFragment.java
@@ -1,13 +1,29 @@
 package com.autorave.chatapp;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -15,6 +31,9 @@ import android.view.ViewGroup;
  */
 public class ContactsFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private ContactsListAdapter contactsListAdapter;
+    private List<User> mContacts;
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -24,8 +43,49 @@ public class ContactsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        recyclerView = view.findViewById(R.id.contacts_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mContacts = new ArrayList<>();
+        getUsers();
+
+        return view;
+    }
+
+    private void getUsers() {
+
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mContacts.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    User user = snapshot.getValue(User.class);
+
+                    if (user != null && firebaseUser != null) {
+                        if (!user.getId().equals(firebaseUser.getUid())) {
+                            mContacts.add(user);
+                        }
+                    }
+                }
+
+                contactsListAdapter = new ContactsListAdapter(mContacts, getContext());
+                recyclerView.setAdapter(contactsListAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
