d5c52fe31f2234bc5bf31351ee8de5196827b898
Petteri Tuononen <monika.musak@gmail.com>
2019-11-14 22:43:02 +0100

    adding chat features

---
+++ app/src/main/java/com/autorave/chatapp/ChatInfo.java
@@ -0,0 +1,42 @@
+package com.autorave.chatapp;
+
+public class ChatInfo {
+
+    private String sender;
+    private String receiver;
+    private String message;
+
+    public ChatInfo(String sender, String receiver, String message) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.message = message;
+    }
+
+    public ChatInfo(){
+
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
