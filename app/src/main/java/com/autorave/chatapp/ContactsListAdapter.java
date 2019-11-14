7c8776abde335a5da80141d1fc468f5b1515a0c5
NicklasPekkarinen <56152106+nicklaspekkarinen@users.noreply.github.com>
2019-11-14 11:21:32 +0100

    Made adapter for contacts list recyclerview

---
+++ app/src/main/java/com/autorave/chatapp/ContactsListAdapter.java
@@ -0,0 +1,77 @@
+package com.autorave.chatapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactsListAdapter extends RecyclerView.Adapter<ContactsListAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mContacts;
+
+    public ContactsListAdapter(List<User> mContacts, Context mContext) {
+
+        this.mContext = mContext;
+        this.mContacts = mContacts;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.contacts_item, parent, false);
+        return new ContactsListAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        final User user = mContacts.get(position);
+        holder.mUserName.setText(user.getUsername());
+        holder.mUserImage.setImageResource(R.mipmap.ic_launcher);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, chatPage.class);
+                intent.putExtra("userId", user.getId());
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView mUserImage;
+        private TextView mUserName;
+
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+            mUserImage = itemView.findViewById(R.id.contacts_image);
+            mUserName = itemView.findViewById(R.id.contacts_username);
+
+        }
+    }
+}
